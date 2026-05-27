0b590d38bf3aed0dfc5f6cf45a86740bba04c46c
krtresi <yuliia.prokhnich@lnu.edu.ua>
2026-05-27 16:38:06 +0300

    Updated project

--- app.py
+++ app.py
@@ -110,6 +110,4 @@ def download_report():
 
 
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
